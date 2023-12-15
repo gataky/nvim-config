b69e436c1d61e5856d52453240c3c83a71f99367
Jeff Oryen <jeffor@zillowgroup.com>
2023-12-14 21:42:37 -0800

    stule harpoon window

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,18 +1,28 @@
 return {
-    -- "gataky/harpoon",
-    -- branch = "harpoon2",
+    "ThePrimeagen/harpoon",
+    enabled = true,
+    branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" },
-    dir = "~/Documents/nvim/harpoon",
-    -- dev = true,
-    config = function(opts)
-        vim.opt.runtimepath:append("~/Documents/nvim/harpoon")
-        require("harpoon"):setup(opts)
+    config = function(_, opts)
+        local harpoon = require("harpoon"):setup(opts)
+        local events = require("harpoon.extensions").event_names
+        harpoon:extend({
+            [events.UI_CREATE] = function(ids)
+                local bg_highlight =
+                    vim.api.nvim_get_hl(0, { name = "Normal" }).bg
+                vim.api.nvim_win_set_option(
+                    ids.win_id,
+                    "winhl",
+                    "Normal:" .. bg_highlight .. ",FloatBorder:Float"
+                )
+            end,
+        })
     end,
     -- stylua: ignore
     keys = {
         { "<leader>a", function() require("harpoon"):list():append() end, desc = "Harpoon Append" },
         { "<leader>h", function()
-                require("harpoon").ui:toggle_quick_menu(require("harpoon"):list(), { border = "rounded", title_pos = "center", bg_highlight="lualine_a_terminal2"})
+                require("harpoon").ui:toggle_quick_menu(require("harpoon"):list(), { border = "rounded", title_pos = "center"})
             end, desc = "Harpoon List" },
         { "<leader>1", function() require("harpoon"):list():select(1) end, desc = "Harpoon #1" },
         { "<leader>2", function() require("harpoon"):list():select(2) end, desc = "Harpoon #2" },
